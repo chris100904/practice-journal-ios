be8e5111ad8f3bca50bdc2247255453936a46e9f
chris100904 <73714518+chris100904@users.noreply.github.com>
2024-12-23 20:47:34 -0500

    Setup coding environment, `.gitignore`, directories, and placeholder README

---
+++ ios/practice-journal-ios/practice-journal-ios/practice_journal_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_journal_iosApp.swift
+//  practice-journal-ios
+//
+//  Created by Christopher Chen on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct practice_journal_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
